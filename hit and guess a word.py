8dd98dc9fab347142b1bc225db7ceace6f7a60d7
anish7410 <anisharaz919@gmail.com>
2023-05-06 16:32:42 +0530

    Removed Commented Code

--- hit and guess a word.py
+++ hit and guess a word.py
@@ -3,7 +3,6 @@ guesses = ""
 print("The Capital City of ITALY is")
 print("NOTE : Please write the first letter in capital\n")
 
-
 hint = ['Rser', 'Rodr', 'Ruge']
 for i in range(3):
     print('HINT : ', hint[i])
@@ -14,29 +13,3 @@ for i in range(3):
     print()
 else:
     print("you LOST")
-
-
-# Your old Code
-# maximum_guesses = 3
-# number_of_guesses = 0
-
-# while guesses != guessing_word:
-#     if number_of_guesses == 0:
-#         print("Hint:Rser")
-#         guesses = input("Guess the capital:")
-#         number_of_guesses += 1
-#     elif number_of_guesses == 1:
-#         print("Hint:Rodr")
-#         print("Guess the capital")
-#         guesses = input("Guess again: ")
-#         number_of_guesses += 1
-#     elif number_of_guesses == 2:
-#         print("Hint:Ruge")
-#         print("Guess the capital ")
-#         guesses = input("Guess again: ")
-#         break
-# if (guesses == guessing_word):
-#     print("You have won")
-
-# else:
-#     print("you have lost")
